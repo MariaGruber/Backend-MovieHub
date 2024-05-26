a54dbaf638ef57b4384c6f934df4d7d7542b16c4
MariaGruber <grubermar1992@gmail.com>
2024-05-27 01:09:45 +0200

    server connected to db

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import app from "./server"
 import config from "./config/config"
+import connect from "./db/db"
 
 console.log("==>", config)
 
 const PORT = config.app.PORT
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+connect().then(() =>
+    app.listen(PORT, () => console.log(`server is running on port ${PORT} and is connected to db`))
+)
+
